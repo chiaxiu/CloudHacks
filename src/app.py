6540f35c01af225ffc17317a1c727c727c5642fc
chiaxiu <146698230+chiaxiu@users.noreply.github.com>
2024-06-22 13:58:31 +0800

    working

--- src/app.py
+++ src/app.py
@@ -1,11 +1,1 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/api/data', methods=['GET'])
-def get_data():
-    data = {'message': 'Hello from Flask!'}
-    return jsonify(data)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+from flask
